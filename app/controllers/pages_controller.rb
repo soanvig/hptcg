09e1fc3c605e386515e9bc302740e3ecf3b9ceca
Schizohatter <soanvig@gmail.com>
2016-12-30 01:24:29 +0100

    Things (see details) - Rescue missing template to status 404 - Add forum page - Fill the gap below .page-content on short pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
     def show
         @page = params[:page]
-        render template: "pages/#{@page}"
+        begin
+            render template: "pages/#{@page}"
+        rescue ActionView::MissingTemplate
+            # template doesn't exist
+            render :status => 404
+        end
     end
 end
