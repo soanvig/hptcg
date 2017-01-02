2ce219d51e4346a4ab8aa261f8b2f39c07fd0cd5
Schizohatter <soanvig@gmail.com>
2017-01-02 13:44:47 +0100

    Polonize links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
     root 'pages#show', page: 'index'
-    get '/contact' => 'contact#form'
-    post '/contact' => 'contact#mail'
+    get '/kontakt' => 'contact#form'
+    post '/kontakt' => 'contact#mail'
+    get '/spis-kart' => 'pages#show', page: 'cardlist'
+    get '/spis-kart-bs' => 'pages#show', page: 'cardlist-bs'
+    get '/spis-kart-da' => 'pages#show', page: 'cardlist-da'
+    get '/spis-kart-qp' => 'pages#show', page: 'cardlist-qp'
+    get '/spolecznosc' => 'pages#show', page: 'community'
+    get '/opis-gry' => 'pages#show', page: 'description'
+    get '/dodatki' => 'pages#show', page: 'extensions'
+    get '/o-grze' => 'pages#show', page: 'game'
+    get '/zasady' => 'pages#show', page: 'rules'
+    get '/handel' => 'pages#show', page: 'trade'
     get '/:page' => 'pages#show'
 end
